e1effb32f078f43619144ab653be27288558c2e8
Nik <136103496+Wolkenfresser@users.noreply.github.com>
2025-03-13 15:25:26 +0300

    Update and rename week3/deposit/deposit.py to week4/deposit.py

--- week4/deposit.py
+++ week4/deposit.py
@@ -5,10 +5,12 @@
 # Группа: ПИЖ-б-о-23-2
 # E-mail: fufaevnikita2015@gmail.com
 
+from typing import Dict, Tuple, List
+
 class TimeDeposit:
     """Абстрактный класс - срочный вклад."""
 
-    def __init__(self, name, interest_rate, period_limit, sum_limit):
+    def __init__(self, name: str, interest_rate: float, period_limit: Tuple[int, int], sum_limit: Tuple[float, float]) -> None:
         """Инициализировать атрибуты класса."""
         self.name = name
         self._interest_rate = interest_rate
@@ -16,7 +18,7 @@ class TimeDeposit:
         self._sum_limit = sum_limit
         self._check_self()
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Вернуть строковое представление депозита."""
         return (f"Наименование:       {self.name}\n"
                 f"Валюта:             {self.currency}\n"
@@ -25,27 +27,28 @@ class TimeDeposit:
                 f"Сумма:              [{self._sum_limit[0]:,.0f}; {self._sum_limit[1]:,.0f})")
 
     @property
-    def currency(self):
+    def currency(self) -> str:
+        """Вернуть валюту вклада."""
         return "руб."  # Не изменяется
 
-    def _check_self(self):
+    def _check_self(self) -> None:
         """Проверить, что данные депозита являются допустимыми."""
         assert 0 < self._interest_rate <= 100, "Неверно указан процент по вкладу!"
         assert 1 <= self._period_limit[0] < self._period_limit[1], "Неверно указаны ограничения по сроку вклада!"
         assert 0 < self._sum_limit[0] <= self._sum_limit[1], "Неверно указаны ограничения по сумме вклада!"
 
-    def _check_user_params(self, initial_sum, period):
+    def _check_user_params(self, initial_sum: float, period: int) -> None:
         """Проверить, что данные депозита соответствуют его ограничениям."""
         is_sum_ok = self._sum_limit[0] <= initial_sum < self._sum_limit[1]
         is_period_ok = self._period_limit[0] <= period < self._period_limit[1]
         assert is_sum_ok and is_period_ok, "Условия вклада не соблюдены!"
 
-    def get_profit(self, initial_sum, period):
+    def get_profit(self, initial_sum: float, period: int) -> float:
         """Вернуть прибыль по вкладу вклада клиента."""
         self._check_user_params(initial_sum, period)
         return initial_sum * self._interest_rate / 100 * period / 12
 
-    def get_sum(self, initial_sum, period):
+    def get_sum(self, initial_sum: float, period: int) -> float:
         """Вернуть сумму вклада клиента после начисления прибыли."""
         return initial_sum + self.get_profit(initial_sum, period)
 
@@ -53,24 +56,24 @@ class TimeDeposit:
 class BonusTimeDeposit(TimeDeposit):
     """Cрочный вклад c получением бонуса к концу срока вклада."""
 
-    def __init__(self, name, interest_rate, period_limit, sum_limit, bonus):
+    def __init__(self, name: str, interest_rate: float, period_limit: Tuple[int, int], sum_limit: Tuple[float, float], bonus: Dict[str, float]) -> None:
         """Инициализировать атрибуты класса."""
         self._bonus = bonus
         super().__init__(name, interest_rate, period_limit, sum_limit)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Вернуть строковое представление депозита."""
         return (super().__str__() + "\n"
                 f"Бонус (%):          {self._bonus['percent']}\n"
                 f"Бонус (мин. сумма): {self._bonus['sum']:,.0f}")
 
-    def _check_self(self):
+    def _check_self(self) -> None:
         """Проверить, что данные депозита являются допустимыми."""
         super()._check_self()
         assert 0 < self._bonus['percent'] <= 100, "Неверно указан бонусный процент!"
         assert 0 < self._bonus['sum'], "Неверно указана минимальная сумма для бонуса!"
 
-    def get_profit(self, initial_sum, period):
+    def get_profit(self, initial_sum: float, period: int) -> float:
         """Вернуть прибыль по вкладу вклада клиента."""
         profit = super().get_profit(initial_sum, period)
         if initial_sum >= self._bonus['sum']:
@@ -81,21 +84,21 @@ class BonusTimeDeposit(TimeDeposit):
 class CompoundTimeDeposit(TimeDeposit):
     """Cрочный вклад c ежемесячной капитализацией процентов."""
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Вернуть строковое представление депозита."""
         return (super().__str__() + "\n"
                 f"Капитализация %:    Да")
 
-    def get_profit(self, initial_sum, period):
+    def get_profit(self, initial_sum: float, period: int) -> float:
         """Вернуть прибыль по вкладу вклада клиента."""
         self._check_user_params(initial_sum, period)
         return initial_sum * (1 + self._interest_rate / 100 / 12) ** period - initial_sum
 
 
 # Список имеющихся депозитов
-deposits_data = dict(interest_rate=5, period_limit=(6, 18), sum_limit=(1000, 100000))
+deposits_data: Dict[str, any] = dict(interest_rate=5, period_limit=(6, 18), sum_limit=(1000, 100000))
 
-deposits = (
+deposits: Tuple[TimeDeposit, ...] = (
     TimeDeposit("Сохраняй", interest_rate=5, period_limit=(6, 18), sum_limit=(1000, 100000)),
     BonusTimeDeposit("Бонусный 2", **deposits_data, bonus=dict(percent=5, sum=2000)),
     CompoundTimeDeposit("С капитализацией", **deposits_data),
