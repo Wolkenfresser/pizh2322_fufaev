595e4164ebba726d5b57c625b17f31c01944edd2
Nik <huhityt@gmail.com>
2025-04-17 10:13:14 +0300

    Initial commit

---
+++ week7/build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Nikita/source/repos/labwork2/tests
+# Build directory: C:/Users/Nikita/source/repos/labwork2/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/Nikita/source/repos/labwork2/build/tests/number_tests[1]_include.cmake")
+subdirs("../_deps/googletest-build")
